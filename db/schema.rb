762188a8e3c1b3253cacffae196f6344476e40e8
jisrag <i.s.r._182@hotmail.com>
2018-10-15 12:51:38 -0500

    Agregamos  el campo user/description

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20181015163253) do
+ActiveRecord::Schema.define(version: 20181015172321) do
 
   create_table "todo_items", force: :cascade do |t|
     t.string "content"
@@ -42,6 +42,7 @@ ActiveRecord::Schema.define(version: 20181015163253) do
     t.datetime "updated_at", null: false
     t.datetime "date_of_birth"
     t.boolean "is_female", default: false
+    t.string "description"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
